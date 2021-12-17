e8993fc87c25cc089f4e057731b5c3d4e7943987
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-12-17 18:09:00 -0500

    Update run.py to accept args,  and start re-write of README

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+# Create redeploy.sh
 echo "./deploy.sh $*" > redeploy.sh
 chmod +x redeploy.sh
 
-docker-compose up
+# default env file
+ENV_FILE='../.env' 
+
+# Create on the fly .env file to pass args into plugin container
+ARGS="$*"
+if [ -n "$ARGS" ];  then
+    tmpfile=$(mktemp)
+    echo ARGS=${ARGS} > $tmpfile
+    ENV_FILE=$tmpfile
+fi
+
+docker-compose --env-file $ENV_FILE up -d
--- plugin_service/run.py
+++ plugin_service/run.py
@@ -1,15 +1,35 @@
+import os
 import nanome
+
 from nanome.service import PluginService
+from nanome.api import Plugin
 
 
 def main():
-    name = '[wip] Browser Plugin.'
-    description = 'Test deploying a Flask application with your plugin.'
-    tags = ['UI']
-    advanced_options = False
-    plugin = nanome.Plugin(name, description, tags, advanced_options)
+    breakpoint()
+    parser = Plugin.create_parser()
+    args, _ = parser.parse_known_args()
+
+    default_name = 'Cookbook'
+    arg_name = args.name or []
+    plugin_name = ' '.join(arg_name) or default_name
+
+    description = 'Calculate and visualize interatomic contacts between small and macro molecules.'
+    tags = ['Interactions']
+
+    plugin = nanome.Plugin(plugin_name, description, tags)
     plugin.set_plugin_class(PluginService)
-    plugin.run()
+
+    # CLI Args take priority over environment variables for NTS settnigs
+    host = args.host or os.environ.get('NTS_HOST')
+    port = args.port or os.environ.get('NTS_PORT') or 0
+
+    configs = {}
+    if host:
+        configs['host'] = host
+    if port:
+        configs['port'] = int(port)
+    plugin.run(**configs)
 
 
 if __name__ == '__main__':
