8b6e7a293804ed1ae59a853b3c360d33172c8bf2
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-12-21 15:07:41 -0500

    Connect notebook to live nanome session(#5)
    - Integrate cookbook with Redis pub sub architecture, allowing tighter integration
    - Add new notebooks for pharmacaphore visualizations and Neo4j integrations
    - Update README
    - Add linter github action

--- README.md
+++ README.md
@@ -1,55 +1,76 @@
 # Nanome Plugin Cookbook
 
-Nanome is an immersive platform for collaborative computationally-driven molecular design. Learn more about Nanome at https://nanome.ai. 
+Nanome is an immersive XR platform for collaborative computationally-driven molecular design. Learn more about Nanome at https://nanome.ai.
 
 
-### Hello! Welcome to the Plugin Chef Cookbook. Here you can find all of the basic miniture code snippets to best cook up a Nanome plugin. 
-As part of the delicious receipes and how-tos for building plugins, here you will find topics ranging from 'How to import structures' to 'How to change representations".
+### Hello! Welcome to the Plugin Chef Cookbook. Here you can find all of the basic miniture code snippets to best cook up a Nanome plugin.
 
 These how-tos are a supplement to the API documentation (https://nanome.readthedocs.io)
 
+A new feature we're testing in this repo is communicating with a running PluginInstance via RPC requests using Redis Pub/Sub, which allows a user interact with their nanome session directly from the web browser (Desktop or VR).
 
-View the Cookbook Jupyter/IPython Notebook directly at https://github.com/nanome-ai/plugin-cookbook/blob/main/notebooks/Cookbook.ipynb
-
-
-Follow the instructions below so you can spin up the Cookbook and run block by block to better understand how to work with Nanome's Python API. 
 
 ## Installation
 
+### Requirements:
+Docker (https://docs.docker.com/get-docker/)
+Docker Compose (https://docs.docker.com/compose/install/)
+
+### Clone, Build,  and deploy
 1) Use Git to clone this repository to your computer.
 ```sh
 git clone https://github.com/nanome-ai/plugin-cookbook.git
 ````
 
-2) Set up pre-commit hook to delete outputs on commmit 
+2) Set up pre-commit hook to clear outputs before commmiting.
 ```sh
 cp pre-commit-hook .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 ```
-### Using Docker
 
-Requires Docker Installed (https://www.docker.com/)
-
-```bash
-docker build . -t cookbook
-docker run -it  -p 8888:8888 cookbook
+3) Create .env file, and populate with NTS variables, and a strong token for your jupyter notebook
+```sh
+cp .env.sample .env
 ```
 
-### Using local python
-
-
-```bash
-conda env update -f jupyter_environ.yml
-npm install
-jupyter lab --ip 0.0.0.0
+3) Build and deploy
+```sh
+./docker/build.sh
+./docker/deploy.sh
 ```
 
-# Get Started
-## Table of Contents:
-<ol>
-	<li>Building your first Plugin. <code>notebooks/Plugins.ipynb</code></li>
-    <li>Utilizing nanome-lib features. <code>notebooks/Cookbook.ipynb</code></li>
-</ol>
+## Using the cookbook
+For now, the cookbook is expected to run on localhost, and you need access to the Docker logs.
+
+- Once the plugin has been started, in the Nanome app you should see "Cookbook" in your Stacks Menu. Select it, and press Activate/Run
+
+- The In-VR web browser should open to your jupyter notebook, with the JUPYTER_TOKEN as a query param.
+	- The url will be available in the plugin_service logs.
+	- you will also need to copy the Redis channel that the plugin instance is polling, and insert that into your cookbook.
+	```
+	Starting PluginService on Redis Channel 2a99a818-005e-4aba-a096-7b27a93c393b
+	```
+- In the notebooks that are Redis compatible (for example, notebooks/Pharmacaphore.ipynb), the first cell will have a variable `redis_channel`, which you set as the uuid from the log. This tells the notebook where your plugin_instance is expecting to receive function requests.
+
+
+## Redis RPC architecture
+- The application contains 2 docker containers deployed using docker-compose, `cookbook`, and `plugin_service`, which communicate via Redis Publish/Subscribe pattern.
+- `plugin_service` container runs your standard plugin instance. When activated in Nanome, the plugin sets a Redis channel name, and opens your web browser to the cookbook. When the plugin is run, it subscribes to the Redis channel, and starts polling, waiting to receive messages containing info on what function to run.
+
+- The `cookbook` container communicates with the plugin_service via the `PluginInstanceRedisInterface`, which resembles the standard PluginInstance class. But instead of communicating with NTS directly, it publishes messages to the set Redis channel that describe what function it wants the plugin instance to run. They message payload is a stringified json blob, where the function args and kwargs are pickled python objects. An example payload looks like:
+```
+{
+  "function": <function_name>
+  "args": [...],
+  "kwargs": {...},
+  "response_channel": "uuid4()"
+}
+```
+- the response channel is subscribed to by `cookbook`, and it awaits the response data.
+- When the message is received by `plugin_service`, it parses it and executes the provided function with args and kwargs. It then publishes the results back the `cookbook`, on the response channel.
+
+![alt text](assets/pubsub.png)
+
 
 ## Contributors
 @mjrosengrant
---
+++ service.py
@@ -0,0 +1,134 @@
+import base64
+import dill
+import io
+import json
+import os
+import redis
+import struct
+import sys
+import uuid
+
+import nanome
+from nanome.util import async_callback, Logs
+from nanome.util.enums import NotificationTypes
+
+BASE_PATH = os.path.dirname(f'{os.path.realpath(__file__)}')
+MENU_PATH = os.path.join(BASE_PATH, 'default_menu.json')
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+
+
+class PluginService(nanome.AsyncPluginInstance):
+
+    @async_callback
+    async def start(self):
+        # Create Redis channel name to send to frontend to publish to
+        permanent_channel = os.environ.get('PERMANENT_CHANNEL')
+        self.redis_channel = permanent_channel if permanent_channel else str(uuid.uuid4())
+        Logs.message(f"Starting {self.__class__.__name__} on Redis Channel {self.redis_channel}")
+
+        # We need to increase the recursion limit in order to properly serialize Complexes
+        recursion_limit = 100000
+        sys.setrecursionlimit(recursion_limit)
+
+        self.streams = []
+        self.shapes = []
+
+    @async_callback
+    async def on_run(self):
+        default_url = os.environ.get('DEFAULT_URL')
+        jupyter_token = os.environ.get('JUPYTER_TOKEN')
+        url = f'{default_url}?token={jupyter_token}'
+        print(f'Opening {url}')
+        self.open_url(url)
+        await self.poll_redis_for_requests(self.redis_channel)
+
+    @async_callback
+    async def poll_redis_for_requests(self, redis_channel):
+        """Start a non-halting loop polling for and processing Plugin Requests.
+
+        Subscribe to provided redis channel, and process any requests received.
+        """
+        rds = redis.Redis(
+            host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD,
+            decode_responses=True)
+        pubsub = rds.pubsub(ignore_subscribe_messages=True)
+        pubsub.subscribe(redis_channel)
+
+        for message in pubsub.listen():
+            if message.get('type') == 'message':
+                try:
+                    data = json.loads(message.get('data'))
+                except json.JSONDecodeError:
+                    error_message = 'JSON Decode Failure'
+                    self.send_notification(NotificationTypes.error, error_message)
+
+                Logs.message(f"Received Request: {data.get('function')}")
+                fn_name = data['function']
+                args = self.unpickle_data(data['args'])
+                kwargs = self.unpickle_data(data['kwargs'])
+                response_channel = data['response_channel']
+
+                function_to_call = getattr(self, fn_name)
+                try:
+                    response = await function_to_call(*args, **kwargs)
+                except (TypeError, RuntimeError) as e:
+                    # TypeError Happens when you await a non-sync function.
+                    # Because nanome-lib doesn't define functions using `async def`,
+                    # I can't find a reliable way to determine whether we need to await asyncs.
+                    # For now, just recall the function without async.
+                    response = function_to_call(*args, **kwargs)
+                except struct.error:
+                    Logs.error(f"Serialization error on {fn_name} call")
+                Logs.message(response)
+                pickled_response = self.pickle_data(response)
+                Logs.message(f'Publishing Response to {response_channel}')
+                rds.publish(response_channel, pickled_response)
+
+    @staticmethod
+    def pickle_data(data):
+        """Return the stringified bytes of pickled data."""
+        bytes_output = io.BytesIO()
+        dill.dump(data, bytes_output)
+        bytes_output_base64 = base64.b64encode(bytes_output.getvalue()).decode()
+        bytes_output.close()
+        return bytes_output_base64
+
+    @staticmethod
+    def unpickle_data(pickled_data):
+        """Unpickle data into its original python version."""
+        pickle_bytes = io.BytesIO(base64.b64decode(pickled_data))
+        unpickled_data = dill.loads(pickle_bytes.read())
+        pickle_bytes.close()
+        return unpickled_data
+
+    async def create_writing_stream(self, indices_list, stream_type, callback=None):
+        """After creating stream, save it for future lookups."""
+        response = await super().create_writing_stream(indices_list, stream_type, callback=callback)
+        stream, _ = response
+        if stream:
+            self.streams.append(stream)
+        return response
+
+    def stream_update(self, stream_id, stream_data):
+        """Function to update stream."""
+        stream = next(strm for strm in self.streams if strm._Stream__id == stream_id)
+        output = stream.update(stream_data)
+        return output
+
+    def stream_destroy(self, stream_id):
+        """Function to destroy stream."""
+        stream = next(strm for strm in self.streams if strm._Stream__id == stream_id)
+        output = stream.destroy()
+        return output
+
+    async def upload_shapes(self, shape_list):
+        for shape in shape_list:
+            print(shape.index)
+        response = await nanome.api.shapes.Shape.upload_multiple(shape_list)
+        self.shapes.extend(response)
+        for shape in shape_list:
+            print(shape.index)
+        return shape_list
