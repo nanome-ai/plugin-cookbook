9d2e73c8d782825607bf12d9ee7742759f782013
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-05-14 02:16:37 -0400

    Add more tests

--- cookbook/schemas.py
+++ cookbook/schemas.py
@@ -2,6 +2,7 @@ import itertools
 import logging
 from marshmallow import Schema, fields, post_load, ValidationError
 from nanome.api import structure
+from nanome.api.user import PresenterInfo
 from nanome.util import Vector3, Quaternion
 
 
@@ -204,12 +205,24 @@ class WorkspaceSchema(Schema):
 
 
 class PresenterInfoSchema(Schema):
-    account_id = fields.Integer()
+    account_id = fields.Str()
     account_name = fields.Str()
     account_email = fields.Email()
     has_org = fields.Bool()
     org_id =fields.Integer()
     org_name = fields.Str()
+
+    @post_load
+    def make_presenter_info(self, data, **kwargs):
+        new_obj = PresenterInfo()
+        for key in data:
+            try:
+                setattr(new_obj, key, data[key])
+            except AttributeError:
+                raise AttributeError('Could not set attribute {}'.format(key))
+        return new_obj
+    
+
 
 
 structure_schema_map = {
--- cookbook/tests.py
+++ cookbook/tests.py
@@ -1,10 +1,11 @@
 import os
 import unittest
-
+import time
 from interface import PluginInstanceRedisInterface
 from schemas import api_function_definitions
 from nanome.api import structure
-
+from nanome.util import enums
+from nanome.api.user import PresenterInfo
 
 # Set up redis credentials
 redis_host = 'redis'
@@ -31,6 +32,7 @@ class APITests(unittest.TestCase):
         comp_list = cls.plugin_instance.request_complex_list()
         cls.test_comp = cls.plugin_instance.request_complexes([comp_list[0].index])[0]
         assert cls.test_comp.index != -1
+        # cls.plugin_instance.center_on_structures([cls.test_comp.index])
 
     def test_request_complex_list(self):
         comp_list = self.plugin_instance.request_complex_list()
@@ -42,7 +44,35 @@ class APITests(unittest.TestCase):
         comp = comp_list[0]
         self.assertTrue(next(comp.atoms, None) is not None)
     
-    @unittest.skip("Not working yet")
+    def test_update_structures_shallow(self):
+        self.test_comp.boxed = True
+        self.plugin_instance.update_structures_shallow([self.test_comp])
+        self.test_comp.boxed = False
+        self.plugin_instance.update_structures_shallow([self.test_comp])
+
+    def test_update_structures_deep(self):
+        for atom in self.test_comp.atoms:
+            atom.set_visible(False)
+        self.plugin_instance.update_structures_deep([self.test_comp])
+        time.sleep(1)
+        for atom in self.test_comp.atoms:
+            atom.set_visible(True)
+        self.plugin_instance.update_structures_deep([self.test_comp])
+
+    @unittest.skip("Not working yet.")
     def test_zoom_on_structure(self):
         # Zoom on complex
-        self.plugin_instance.zoom_on_structures(self.test_comp.index)
+        self.plugin_instance.zoom_on_structures([self.test_comp])
+
+    def test_send_notification(self):
+        notification_type = enums.NotificationTypes.success
+        self.plugin_instance.send_notification(notification_type, "Test notification Successful")
+    
+    def test_open_url(self):
+        url = 'https://nanome.ai'
+        self.plugin_instance.open_url(url)
+    
+    def test_request_presenter_info(self):
+        presenter_info = self.plugin_instance.request_presenter_info()
+        breakpoint()
+        self.assertTrue(isinstance(presenter_info, PresenterInfo))
