90ef7f4942038b6f62993fa41fc7d8fe5827c333
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-03-06 17:02:37 -0500

    properly install redis-interface

--- redis-interface/redis_interface/interface.py
+++ redis-interface/redis_interface/interface.py
@@ -7,7 +7,9 @@ from nanome import PluginInstance
 from nanome.api import schemas
 from nanome.util import Logs
 from marshmallow import fields
-from api_definitions import api_function_definitions
+from .api_definitions import api_function_definitions
+
+__all__ = ['PluginInstanceRedisInterface', 'StreamRedisInterface']
 
 
 class StreamRedisInterface:
--- redis_interface/setup.py
+++
@@ -1,7 +0,0 @@
-# Setup.py for python lib
-
-from distutils.core import setup
-setup(name='redis_interface',
-      version='0.1',
-      py_modules=['redis_interface'],
-      )
